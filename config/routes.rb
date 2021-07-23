20efc7f07b3effb7914b5060f65a8b37c9912d16
BIODUN <abiodun@launchpadrecruits.com>
2021-07-23 15:46:48 +0100

    added form to assign book to a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'books#home'
   resources :books
-  resources :users
+  resources :users do
+    post '/assign_book', to: 'users#assign'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
