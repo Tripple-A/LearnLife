aecc4f2a40fa37155d35479396cd025f99182846
BIODUN <abiodun@launchpadrecruits.com>
2021-07-24 13:22:04 +0100

    converted erb to haml files

--- app/models/assigned_book.rb
+++ app/models/assigned_book.rb
@@ -4,4 +4,14 @@ class AssignedBook < ApplicationRecord
   belongs_to :borrower, class_name: 'User'
   belongs_to :book
   validates_presence_of :return_date
+  before_save :confirm_availability
+
+  def confirm_availability
+    book_id = self.book_id
+    available = Book.find(book_id).available
+    return if available
+
+    errors.add(:book_id, 'this book is unavailable')
+    throw :abort
+  end
 end
